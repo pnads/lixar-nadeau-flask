710832760706a95900a7010d82481ba85f4f49b4
Patrick Nadeau <patrick.a.nadeau@gmail.com>
2020-11-09 13:39:55 -0500

    Added download and dashboard views; rebuilt pipenv

--- lixar_flask/views.py
+++ lixar_flask/views.py
@@ -2,4 +2,13 @@ from lixar_flask import app
 
 @app.route("/")
 def home_view():
-    return "<h1>Hello world!</h1>"
+    return "<h1>Home</h1>"
+
+@app.route("/download/")
+def download_view():
+    return "<h1>Download</h1>"
+
+
+@app.route("/dashboard/")
+def dashboard_view():
+    return "<h1>Dashboard</h1>"
