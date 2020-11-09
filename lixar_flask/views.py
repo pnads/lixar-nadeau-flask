49a24948e8bfd7476ac6b37e052b93443b4c0d12
Patrick Nadeau <patrick.a.nadeau@gmail.com>
2020-11-09 13:48:27 -0500

    Added base, home templates

--- lixar_flask/views.py
+++ lixar_flask/views.py
@@ -1,13 +1,13 @@
+from flask import render_template
 from lixar_flask import app
 
 @app.route("/")
 def home_view():
-    return "<h1>Home</h1>"
+    return render_template('home.html')
 
 @app.route("/download/")
 def download_view():
     return "<h1>Download</h1>"
-
 
 @app.route("/dashboard/")
 def dashboard_view():
